0e465c858dcc61f19fad01bdb8413df4dc9ab6d2
Filip Š <filip.stamcar@hotmail.com>
2018-01-24 19:10:41 +0100

    Update package details in setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,42 @@
 from setuptools import setup
 
-
 def readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
-
-setup(name='jakopicevca',
-      version='1.0',
-      description='Program for Astro Pi 2017/18 - Mission Space Lab, team Jakopičevca.',
-      long_description=readme(),
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Operating System :: Unix',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Astronomy',
-      ],
-      keywords='Astro Pi, Mission Space Lab, OŠ Riharda Jakopiča',
-      url='http://github.com/filips123/jakopicevca',
-      author='Team Jakopičevca',
-      author_email='filip.stamcar@hotmail.com',
-      license='GPLv3+',
-      packages=['jakopicevca'],
-      platforms='Linux',
-      install_requires=[
-          'ephem'
-      ],
-      python_requires='>=3',
-      include_package_data=True)
+setup(
+    name = "jakopicevca",
+    version = "1.0",
+    description = "Program for Astro Pi 2017/18 - Mission Space Lab - Team Jakopičevca",
+    long_description = readme(),
+    license = "GPLv3+",
+    
+    packages = ["jakopicevca"],
+    
+    entry_points = {
+        "console_scripts": ["jakopicevca=jakopicevca.__main__"],
+    },
+    
+    install_requires = [
+        "ephem"
+    ],
+    
+    author = "Team Jakopičevca",
+    author_email = "filip.stamcar@hotmail.com",
+    url = "https://github.com/filips123/jakopicevca/",
+    keywords = "Raspberry Pi, Astro Pi, Mission Space Lab, OŠ Riharda Jakopiča, ESA",
+    platforms = "Linux",
+    
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Astronomy"
+    ],
+    
+    include_package_data = True
+)
